7a7c57520428065e54b1d63056d74c7875b56d4e
时之虫 <62002934+ToKiNoBug@users.noreply.github.com>
2021-10-06 22:25:14 +0800

    Update BlockListManager_zh_CN.ts

--- BlockListManager/BlockListManager_zh_CN.ts
+++ BlockListManager/BlockListManager_zh_CN.ts
@@ -6,24 +6,26 @@
     <message>
         <location filename="TokiBlock.cpp" line="31"/>
         <source>错误：方块对应的图像不存在或不可用</source>
-        <translation type="unfinished"></translation>
+        <translation>Error: image of a block not found</translation>
     </message>
     <message>
         <location filename="TokiBlock.cpp" line="32"/>
         <source>方块id：</source>
-        <translation type="unfinished"></translation>
+        <translation>Block id: </translation>
     </message>
     <message>
         <location filename="TokiBlock.cpp" line="32"/>
         <source>
 缺失的图像：</source>
-        <translation type="unfinished"></translation>
+        <translation>
+Missing image file: </translation>
     </message>
     <message>
         <location filename="TokiBlock.cpp" line="33"/>
         <source>
 你可以点击Yes忽略这个错误，点击YesToAll屏蔽同类的警告，或者点击Close结束程序</source>
-        <translation type="unfinished"></translation>
+        <translation>
+Click Yes to ignore this error; Click YesToAll to ignore all these warnings; Click Close to exit.</translation>
     </message>
 </context>
 </TS>
