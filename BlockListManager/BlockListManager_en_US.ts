2dceb4ac43f5bc32d996f19b13ea061ab07c28c0
TokiNoBug <tokinobug@163.com>
2022-08-22 22:48:02 +0800

    update .ts files for BlockListManager and imageCutter

--- BlockListManager/BlockListManager_en_US.ts
+++ BlockListManager/BlockListManager_en_US.ts
@@ -2,25 +2,56 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
 <context>
+    <name>BlockListManager</name>
+    <message>
+        <source>预设文件格式错误</source>
+        <translation>Wrong preset files: syntax error!</translation>
+    </message>
+    <message>
+        <source>解析预设文件时遇到json格式错误：</source>
+        <translation>Json error when parsing preset file:</translation>
+    </message>
+    <message>
+        <source>预设文件中包含未知方块</source>
+        <translation>Preset file contains unknown block.</translation>
+    </message>
+    <message>
+        <source>预设文件中基色</source>
+        <translation>In the preset file, base color </translation>
+    </message>
+    <message>
+        <source>对应的方块id</source>
+        <translation>corresponding block id </translation>
+    </message>
+    <message>
+        <source>在方块列表中不存在</source>
+        <translation>doesn&apos;t exist in the block list.</translation>
+    </message>
+    <message>
+        <source>将为这个启用默认选项</source>
+        <translation>SlopeCraft will use the default option for this.</translation>
+    </message>
+</context>
+<context>
     <name>TokiBlock</name>
     <message>
         <source>错误：方块对应的图像不存在或不可用</source>
-        <translation type="vanished">Error: image of a block not found</translation>
+        <translation>Error: image of a block not found</translation>
     </message>
     <message>
         <source>方块id：</source>
-        <translation type="vanished">Block id: </translation>
+        <translation>Block id: </translation>
     </message>
     <message>
         <source>
 缺失的图像：</source>
-        <translation type="vanished">
+        <translation>
 Missing image file: </translation>
     </message>
     <message>
         <source>
 你可以点击Yes忽略这个错误，点击YesToAll屏蔽同类的警告，或者点击Close结束程序</source>
-        <translation type="vanished">
+        <translation>
 Click Yes to ignore this error; Click YesToAll to ignore all these warnings; Click Close to exit.</translation>
     </message>
 </context>
--- BlockListManager/BlockListManager_zh_CN.ts
+++ BlockListManager/BlockListManager_zh_CN.ts
@@ -2,25 +2,56 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
 <context>
+    <name>BlockListManager</name>
+    <message>
+        <source>预设文件格式错误</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>解析预设文件时遇到json格式错误：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>预设文件中包含未知方块</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>预设文件中基色</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>对应的方块id</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>在方块列表中不存在</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>将为这个启用默认选项</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>TokiBlock</name>
     <message>
         <source>错误：方块对应的图像不存在或不可用</source>
-        <translation type="vanished">Error: image of a block not found</translation>
+        <translation>Error: image of a block not found</translation>
     </message>
     <message>
         <source>方块id：</source>
-        <translation type="vanished">Block id: </translation>
+        <translation>Block id: </translation>
     </message>
     <message>
         <source>
 缺失的图像：</source>
-        <translation type="vanished">
+        <translation>
 Missing image file: </translation>
     </message>
     <message>
         <source>
 你可以点击Yes忽略这个错误，点击YesToAll屏蔽同类的警告，或者点击Close结束程序</source>
-        <translation type="vanished">
+        <translation>
 Click Yes to ignore this error; Click YesToAll to ignore all these warnings; Click Close to exit.</translation>
     </message>
 </context>
