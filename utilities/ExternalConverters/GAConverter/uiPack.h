ab7e7a9a8ec8d84eecace351a36b05448b8b6c45
TokiNoBug <tokinobug@163.com>
2022-10-10 13:52:56 +0800

    recover uiPack

---
+++ utilities/ExternalConverters/GAConverter/uiPack.h
@@ -0,0 +1,50 @@
+/*
+ Copyright © 2021-2022  TokiNoBug
+This file is part of SlopeCraft.
+
+    SlopeCraft is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SlopeCraft is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SlopeCraft.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact with me:
+    github:https://github.com/ToKiNoBug
+    bilibili:https://space.bilibili.com/351429231
+*/
+
+#ifndef UIPACK_H
+#define UIPACK_H
+#include "GACvterDefines.hpp"
+#include "sortColor.h"
+
+#include <HeuristicFlow/SimpleMatrix>
+
+#include <uiPack/uiPack.h>
+
+namespace GACvter {
+
+using uiPack = ::uiPack;
+
+struct CvterInfo {
+  EImage rawImageCache;
+  heu::MatrixDynamicSize<sortColor>
+      colorMap; //  the relationship between pixels and sortColor s
+  std::clock_t prevClock;
+  bool strongMutation;
+  GrayImage edgeFeatureMap;
+  std::vector<Var_t> seeds; //     seeds to initialize population
+  std::unordered_map<ARGB, order_t> iniTool;
+  uiPack ui;
+};
+
+} //  namespace GACvter
+
+#endif // UIPACK_H
---
+++ utilities/uiPack/uiPack.h
@@ -0,0 +1,43 @@
+/*
+ Copyright © 2021-2022  TokiNoBug
+This file is part of SlopeCraft.
+
+    SlopeCraft is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SlopeCraft is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SlopeCraft.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact with me:
+    github:https://github.com/ToKiNoBug
+    bilibili:https://space.bilibili.com/351429231
+*/
+
+#ifndef SCL_UIPACK_UIPACK_H
+#define SCL_UIPACK_UIPACK_H
+
+struct uiPack {
+public:
+  void *_uiPtr{nullptr};
+  void (*progressRangeSet)(void *, int, int, int){nullptr};
+  void (*progressAdd)(void *, int){nullptr};
+
+public:
+  inline void rangeSet(int a, int b, int c) const noexcept {
+    if (progressRangeSet != nullptr)
+      progressRangeSet(_uiPtr, a, b, c);
+  }
+  inline void add(int d) const noexcept {
+    if (progressAdd != nullptr)
+      progressAdd(_uiPtr, d);
+  }
+};
+
+#endif // SCL_UIPACK_UIPACK_H
