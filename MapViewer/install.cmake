031868f827fbf66e6755dd39eb2c53d6d8da3638
虫 <tokinobug@163.com>
2023-03-25 11:01:51 +0800

    fix the installation script for mapViewer on mac

--- MapViewer/install.cmake
+++ MapViewer/install.cmake
@@ -8,7 +8,7 @@ if(CMAKE_SYSTEM_NAME MATCHES "Windows")
 endif()
 
 if(CMAKE_SYSTEM_NAME MATCHES "Linux")
-    #set_target_properties(MapViewer PROPERTIES INSTALL_RPATH "../lib")
+    # set_target_properties(MapViewer PROPERTIES INSTALL_RPATH "../lib")
     install(TARGETS MapViewer
         RUNTIME DESTINATION bin
         BUNDLE DESTINATION lib
@@ -20,14 +20,13 @@ if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
     install(TARGETS MapViewer
         RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}
         BUNDLE DESTINATION ${CMAKE_INSTALL_PREFIX}
-
-        # Install icon for macOS
-        # MapViewer.app
-        file(GLOB MapViewer_Icon ${CMAKE_SOURCE_DIR}/MapViewer/others/MapViewer.icns)
-        install(FILES ${MapViewer_Icon}
-        DESTINATION ${CMAKE_INSTALL_PREFIX}/MapViewer.app/Contents/Resources)
     )
-endif()
 
+    # Install icon for macOS
+    # MapViewer.app
+    file(GLOB MapViewer_Icon ${CMAKE_SOURCE_DIR}/MapViewer/others/MapViewer.icns)
+    install(FILES ${MapViewer_Icon}
+        DESTINATION ${CMAKE_INSTALL_PREFIX}/MapViewer.app/Contents/Resources)
+endif()
 
 message(WARNING "No rule to install MapViewer, because the system is not Windows, linux or MacOS.")
