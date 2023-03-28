0a0d9417ed303b5524c675802482542a7e0e0f22
ToKiNoBug <tokinobug@163.com>
2023-03-28 12:37:00 +0800

    add preset files for 4 internal presets

--- SlopeCraftMain/install.cmake
+++ SlopeCraftMain/install.cmake
@@ -2,10 +2,15 @@
 file(GLOB SlopeCraft_install_jsons
     "${CMAKE_SOURCE_DIR}/Blocks/*.json"
     "${CMAKE_SOURCE_DIR}/Blocks/*.md")
+
 file(GLOB SlopeCraft_install_png_fixedblocks
     "${CMAKE_SOURCE_DIR}/Blocks/FixedBlocks/*.png")
+
 file(GLOB SlopeCraft_install_png_customblocks
     "${CMAKE_SOURCE_DIR}/Blocks/CustomBlocks/*.png")
+
+file(GLOB SlopeCraft_install_presets
+    "${CMAKE_CURRENT_SOURCE_DIR}/others/*.sc_preset_json")
 
 if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
     # Install for macOS
@@ -38,6 +43,12 @@ if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
         DESTINATION ${CMAKE_INSTALL_PREFIX}/SlopeCraft.app/Contents/MacOS/Blocks/CustomBlocks)
     install(FILES ${SlopeCraft_install_png_customblocks}
         DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/SlopeCraft.app/Contents/MacOS/Blocks/CustomBlocks)
+    
+    # Install presets
+    install(FILES ${SlopeCraft_install_presets} 
+        DESTINATION ${CMAKE_INTALL_PREFIX}/SlopeCraft.app/Contents/MasOS/Blocks/Presets)
+    install(FILES ${SlopeCraft_install_presets} 
+        DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/SlopeCraft.app/Contents/MasOS/Blocks/Presets)
     return()
 endif()
 
@@ -75,6 +86,12 @@ if(CMAKE_SYSTEM_NAME MATCHES "Windows")
         DESTINATION ${CMAKE_INSTALL_PREFIX}/Blocks/CustomBlocks)
     install(FILES ${SlopeCraft_install_png_customblocks}
         DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/Blocks/CustomBlocks)
+    
+    # Install presets
+    install(FILES ${SlopeCraft_install_presets} 
+        DESTINATION ${CMAKE_INTALL_PREFIX}/Blocks/Presets)
+    install(FILES ${SlopeCraft_install_presets} 
+        DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/Blocks/Presets)
 
     return()
 endif()
@@ -109,6 +126,12 @@ if(CMAKE_SYSTEM_NAME MATCHES "Linux")
 
     install(FILES ${SlopeCraft_Icon}
         DESTINATION bin/icons)
+        
+    # Install presets
+    install(FILES ${SlopeCraft_install_presets} 
+        DESTINATION bin/Blocks/Presets)
+    install(FILES ${SlopeCraft_install_presets} 
+        DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/Blocks/Presets)
 
     return()
 endif()
