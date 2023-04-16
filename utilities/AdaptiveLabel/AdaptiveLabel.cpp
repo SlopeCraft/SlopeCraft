f3bf62d821fe9f47b48dcc11ae5351ebae1169cb
ToKiNoBug <tokinobug@163.com>
2023-04-16 19:28:01 +0800

    Add adaptive label

---
+++ utilities/AdaptiveLabel/AdaptiveLabel.cpp
@@ -0,0 +1,41 @@
+#include "AdaptiveLabel.h"
+#include <QWidget>
+#include <QImage>
+#include <cmath>
+
+AdaptiveLabel::AdaptiveLabel(QWidget* parent) : QLabel(parent) {}
+
+// AdaptiveLabel::~AdaptiveLabel() {}
+
+QPixmap AdaptiveLabel::pixmap() const noexcept { return this->real_image; }
+
+void AdaptiveLabel::setPixmap(const QPixmap& img) noexcept {
+  this->real_image = img;
+
+  this->refresh_image();
+}
+
+void AdaptiveLabel::refresh_image() noexcept {
+  const auto raw_image_size = this->real_image.size();
+
+  if (raw_image_size.height() <= 0 || raw_image_size.width() <= 0) {
+    static_cast<QLabel*>(this)->setPixmap(QPixmap{});
+    return;
+  }
+  auto label_size = this->size();
+
+  label_size.setHeight(std::max(label_size.height(), 1));
+  label_size.setWidth(std::max(label_size.width(), 1));
+
+  QPixmap scaled =
+      this->real_image.scaled(label_size, Qt::AspectRatioMode::KeepAspectRatio,
+                              Qt::TransformationMode::SmoothTransformation);
+
+  static_cast<QLabel*>(this)->setPixmap(scaled);
+}
+
+void AdaptiveLabel::resizeEvent(QResizeEvent* event) noexcept {
+  QLabel::resizeEvent(event);
+
+  this->refresh_image();
+}
---
+++ utilities/AdaptiveLabel/AdaptiveLabel.h
@@ -0,0 +1,35 @@
+#include <QLabel>
+#include <QPixmap>
+#include <QResizeEvent>
+
+class AdaptiveLabel : public QLabel {
+  // Q_OBJECT
+ private:
+  QPixmap real_image;
+
+  void refresh_image() noexcept;
+
+ public:
+  explicit AdaptiveLabel(QWidget* parent);
+  ~AdaptiveLabel() = default;
+
+  void setPixmap(const QPixmap&) noexcept;
+
+  QPixmap pixmap() const noexcept;
+
+  QLabel& base() noexcept { return *this; }
+  const QLabel& base() const noexcept { return *this; }
+
+  void resizeEvent(QResizeEvent* event) noexcept override;
+
+  // using QLabel::;
+  using QLabel::setAlignment;
+  using QLabel::setFrameShape;
+  using QLabel::setMinimumSize;
+  using QLabel::setObjectName;
+  using QLabel::setScaledContents;
+  using QLabel::setSizePolicy;
+  using QLabel::setStyleSheet;
+  using QLabel::setText;
+  using QLabel::sizePolicy;
+};
