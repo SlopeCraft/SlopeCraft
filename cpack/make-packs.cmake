70ddab3287407006f745dc619ed736c391a87273
虫 <tokinobug@163.com>
2023-04-26 19:33:38 +0800

    improve windployqt working with cpack

--- cpack/make-packs.cmake
+++ cpack/make-packs.cmake
@@ -11,16 +11,6 @@ if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
     set(CPACK_PACKAGE_NAME ${CPACK_PACKAGE_NAME}-debug)
 endif()
 
-# set(CPACK_PACKAGE_NAME ${CPACK_PACKAGE_NAME}-${SlopeCraft_GPU_API})
-if(${WIN32})
-    configure_file(${CMAKE_SOURCE_DIR}/cpack/pack_deployed_files.cmake.in
-        ${CMAKE_CURRENT_BINARY_DIR}/pack_deployed_files.cmake
-        @ONLY)
-    list(APPEND CPACK_PRE_BUILD_SCRIPTS ${CMAKE_CURRENT_BINARY_DIR}/pack_deployed_files.cmake)
-
-    # set(CPACK_PACKAGE_FILES)
-endif()
-
 include(${CMAKE_SOURCE_DIR}/cpack/deb.cmake)
 
 include(CPack)
