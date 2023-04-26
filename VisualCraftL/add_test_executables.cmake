dcb7b9091138f8b9b052551dc1a812544ab7d129
虫 <tokinobug@163.com>
2023-04-26 19:34:03 +0800

    link to Eigen by interface targets

--- VisualCraftL/add_test_executables.cmake
+++ VisualCraftL/add_test_executables.cmake
@@ -27,8 +27,7 @@ add_executable(test_VCL_project_image tests/test_VCL_project_image.cpp)
 target_link_libraries(test_VCL_project_image PRIVATE VisualCraftL PNG::PNG)
 target_include_directories(test_VCL_project_image PRIVATE
     ${cli11_include_dir}
-    ${SlopeCraft_Nlohmann_json_include_dir}
-    ${SlopeCraft_Eigen3_include_dir})
+    ${SlopeCraft_Nlohmann_json_include_dir})
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/test_project_image)
 
 # test showing model
@@ -36,8 +35,7 @@ add_executable(test_VCL_model tests/test_VCL_model.cpp)
 target_link_libraries(test_VCL_model PRIVATE VisualCraftL)
 target_include_directories(test_VCL_model PRIVATE
     ${cli11_include_dir}
-    ${SlopeCraft_Nlohmann_json_include_dir}
-    ${SlopeCraft_Eigen3_include_dir})
+    ${SlopeCraft_Nlohmann_json_include_dir})
 
 # internal test for rotation
 add_executable(itest_VCL_rotate tests/itest_VCL_rotate.cpp)
@@ -59,7 +57,5 @@ include(generate_tests.cmake)
 add_executable(test_VCL_version tests/test_VCL_version.cpp)
 target_link_libraries(test_VCL_version PRIVATE VisualCraftL)
 target_compile_features(test_VCL_version PRIVATE cxx_std_17)
-
-
 
 add_test(NAME test_VCL_version COMMAND test_VCL_version WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
--- cmake/find_Eigen3.cmake
+++ cmake/find_Eigen3.cmake
@@ -1,74 +1,18 @@
 set(SlopeCraft_Eigen3_found OFF)
+find_package(Eigen3 3.4.0)
 
-function (SlopeCraft_examine_Eigen3_include_dir)
-    if(NOT DEFINED SlopeCraft_Eigen3_include_dir)
-        set(SlopeCraft_Eigen3_found OFF)
-
-    else()
-        # examine whether the input is valid
-        if(EXISTS ${SlopeCraft_Eigen3_include_dir}/Eigen/Dense)
-            # the assigned Eigen3_include_dir is valid. nothing to do.
-            message("The value of SlopeCraft_Eigen3_include_dir is valid. SlopeCraft will have access to Eigen3")
-            #set(SlopeCraft_download_Eigen340 OFF)
-            set(SlopeCraft_Eigen3_found ON)
-            
-        else()
-            message(WARNING 
-                "The value of SlopeCraft_Eigen3_include_dir is invalid. Failed to find file : "
-                ${SlopeCraft_Eigen3_include_dir}/Eigen/Dense)
-            #set(SlopeCraft_download_Eigen340 ON)
-            set(SlopeCraft_Eigen3_found OFF)
-        endif()
-    endif()
-    
-endfunction()
-
-
-SlopeCraft_examine_Eigen3_include_dir()
-
-    # try to find Eigen3 by find_package
-if(NOT ${SlopeCraft_Eigen3_found})
-    find_package(Eigen3 3.4)
-
-    if(${EIGEN3_FOUND})
-        message(STATUS "CMake have found an libEigen by find_package")
-        set(SlopeCraft_Eigen3_include_dir ${EIGEN3_INCLUDE_DIR})
-        set(SlopeCraft_Eigen3_found ON)
-    endif()
+if(${Eigen3_FOUND})
+    message(STATUS "EIGEN3_INCLUDE_DIR = ${EIGEN3_INCLUDE_DIR}")
+    return()
 endif()
 
+include(FetchContent)
 
+FetchContent_Declare(Eigen3
+    GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+    GIT_TAG 3.4.0
+    OVERRIDE_FIND_PACKAGE)
 
-# try to find Eigen3 in the 3rdParty directory
-if(NOT ${SlopeCraft_Eigen3_found})
-    if(EXISTS ${CMAKE_SOURCE_DIR}/3rdParty/eigen/Eigen/Dense)
-        message(STATUS "Found Eigen in 3rdParty/eigen")
-        set(SlopeCraft_Eigen3_include_dir ${CMAKE_SOURCE_DIR}/3rdParty/eigen)
-        set(SlopeCraft_Eigen3_found ON)
-    endif()
-endif()
+FetchContent_MakeAvailable(Eigen3)
 
-
-if(NOT ${SlopeCraft_Eigen3_found})
-    message(STATUS "Cloning Eigen...")
-    execute_process(
-        COMMAND git clone https://gitlab.com/libeigen/eigen.git
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/3rdParty
-        COMMAND_ERROR_IS_FATAL ANY
-        ECHO_ERROR_VARIABLE
-        ECHO_OUTPUT_VARIABLE
-    )
-
-    message(STATUS "Successfully cloned Eigen. Checking out to tag 3.4.0")
-    execute_process(
-        COMMAND git checkout 3.4.0
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/3rdParty/eigen
-        COMMAND_ERROR_IS_FATAL ANY
-        ECHO_ERROR_VARIABLE
-        ECHO_OUTPUT_VARIABLE
-    )
-    message(STATUS "Eigen v3.4.0 is ready")
-
-    set(SlopeCraft_download_Eigen340 OFF)
-    set(SlopeCraft_Eigen3_include_dir ${CMAKE_SOURCE_DIR}/3rdParty/eigen)
-endif()
+find_package(Eigen3 3.4.0 REQUIRED)
