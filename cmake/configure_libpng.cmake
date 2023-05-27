7e50c81ba076414a602f5d659db1b594a785885e
虫 <tokinobug@163.com>
2023-05-27 13:22:44 +0800

    fix macos find png

--- cmake/configure_libpng.cmake
+++ cmake/configure_libpng.cmake
@@ -4,18 +4,3 @@ if(${APPLE})
 endif()
 
 find_package(PNG 1.6 REQUIRED)
-
-if(${PNG_FOUND})
-    return()
-endif()
-
-include(FetchContent)
-
-FetchContent_Declare(PNG
-    GIT_REPOSITORY https://github.com/glennrp/libpng
-    GIT_TAG v1.6.39
-    OVERRIDE_FIND_PACKAGE)
-
-FetchContent_MakeAvailable(PNG)
-
-find_package(PNG 1.6 REQUIRED CONFIG)
