dc8ad32e2c134b8961cbeb07709a8c7e08b4a354
虫 <tokinobug@163.com>
2023-05-31 21:08:59 +0800

    fixed fmtlib compiling with clang

--- cmake/configure_fmtlib.cmake
+++ cmake/configure_fmtlib.cmake
@@ -1,28 +1,39 @@
 cmake_minimum_required(VERSION 3.14)
 
-include(FetchContent)
-
 set(SC_CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
 
-set(CMAKE_CXX_FLAGS "-fPIC")
+set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
 
-FetchContent_Declare(
-  fmt
-  GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-  GIT_TAG "9.1.0"
-  OVERRIDE_FIND_PACKAGE
+find_package(fmt)
 
-  # QUIET false
-  # FETCHCONTENT_BASE_DIR ${CMAKE_BINARY_DIR}/3rdParty
-  # FETCHCONTENT_TRY_FIND_PACKAGE_MODE ALWAYS FIND_PACKAGE_ARGS find_fmt_args
-)
+if(NOT ${fmt_FOUND})
+  include(FetchContent)
+  FetchContent_Declare(
+    fmt
+    GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+    GIT_TAG "9.1.0"
+    OVERRIDE_FIND_PACKAGE
 
-message(STATUS "Downaloding and building fmtlib......")
+    # QUIET false
+    # FETCHCONTENT_BASE_DIR ${CMAKE_BINARY_DIR}/3rdParty
+    # FETCHCONTENT_TRY_FIND_PACKAGE_MODE ALWAYS FIND_PACKAGE_ARGS find_fmt_args
+  )
 
-FetchContent_MakeAvailable(fmt)
+  message(STATUS "Downaloding and building fmtlib......")
+
+  FetchContent_MakeAvailable(fmt)
+  find_package(fmt REQUIRED)
+endif()
 
 set(CMAKE_CXX_FLAGS ${SC_CMAKE_CXX_FLAGS})
 unset(SC_CMAKE_CXX_FLAGS)
+
+if(${MSVC})
+  # enable c++ exceptions
+  add_compile_options("/EHsc")
+
+  # target_compile_options(fmt::fmt INTERFACE "/EHsc")
+endif()
 
 # message(STATUS "fmt_POPULATED = " )
 
