98319207192c6367567addf2203d38807f5cd0c7
虫 <tokinobug@163.com>
2023-07-01 15:51:19 +0800

    fix VCL vanilla zips downloading

--- cmake/configure_vanilla_zips.cmake
+++ cmake/configure_vanilla_zips.cmake
@@ -8,7 +8,7 @@ set(VCL_zip_names
         "Vanilla_1_18_2.zip"
         "Vanilla_1_19_3.zip"
         "Vanilla_1_20_1.zip")
-
+#https://github.com/SlopeCraft/VisualCraft-binaries/releases/download/resource-packs/Vanilla_1_20_1.zip
 set(VCL_url_prefix "https://github.com/SlopeCraft/VisualCraft-binaries/releases/download/resource-packs/")
 
 foreach (mcver RANGE 12 20)
@@ -26,13 +26,14 @@ foreach (mcver RANGE 12 20)
 
     message(STATUS "${VCL_current_zip_name} not found. Downloading...")
 
+    string(CONCAT VCL_current_download_url ${VCL_url_prefix} ${VCL_current_zip_name})
     file(DOWNLOAD
-            ${VCL_url_prefix} ${VCL_current_zip_name}
+            ${VCL_current_download_url}
             ${CMAKE_SOURCE_DIR}/binaries/${VCL_current_zip_name})
 
     if (EXISTS ${CMAKE_SOURCE_DIR}/binaries/${VCL_current_zip_name})
         message(STATUS "Downloaded resoruce pack for MC${mcver}.")
     else ()
-        message(FATAL_ERROR "Failed to download resource pack for MC${mcver}.")
+        message(FATAL_ERROR "Failed to download resource pack for MC${mcver} from ${VCL_current_download_url}.")
     endif ()
 endforeach (mcver RANGE 12 20)
