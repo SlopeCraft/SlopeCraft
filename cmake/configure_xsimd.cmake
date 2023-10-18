8534c147fda40239808b8fccd5bb667dea4969ae
虫 <tokinobug@163.com>
2023-10-18 11:34:50 +0800

    change the way of introducing xsimd

--- cmake/configure_xsimd.cmake
+++ cmake/configure_xsimd.cmake
@@ -1,10 +1,15 @@
-include(FetchContent)
+find_package(xsimd QUIET)
+if (NOT ${xsimd_FOUND})
+    message(FATAL_ERROR "xsimd is not installed. You may install it with vcpkg")
+endif ()
 
-FetchContent_Declare(xsimd
-    GIT_REPOSITORY https://github.com/xtensor-stack/xsimd.git
-    GIT_TAG 11.1.0
-    OVERRIDE_FIND_PACKAGE)
-
-FetchContent_MakeAvailable(xsimd)
-
-find_package(xsimd REQUIRED)
+#include(FetchContent)
+#
+#FetchContent_Declare(xsimd
+#    GIT_REPOSITORY https://github.com/xtensor-stack/xsimd.git
+#    GIT_TAG 11.1.0
+#    OVERRIDE_FIND_PACKAGE)
+#
+#FetchContent_MakeAvailable(xsimd)
+#
+#find_package(xsimd REQUIRED)
