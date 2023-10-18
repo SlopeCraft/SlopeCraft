bbcefba01ac19216fdce967183059e5416c2baf3
虫 <tokinobug@163.com>
2023-10-18 14:16:51 +0800

    enable neon

---
+++ cmake/select_vectorize_flag.cmake
@@ -0,0 +1,24 @@
+
+include(CheckCXXCompilerFlag)
+#add_definitions("-DSC_VECTORIZE_AVX2")
+
+message(STATUS "System processor arch = ${CMAKE_SYSTEM_PROCESSOR}")
+
+if (${CMAKE_SYSTEM_PROCESSOR} STREQUAL "AMD64")
+    if (${MSVC})
+        set(SlopeCraft_vectorize_flags "/arch:AVX2")
+    else ()
+        set(SlopeCraft_vectorize_flags -mavx -mavx2 -mfma)
+    endif ()
+elseif (${CMAKE_SYSTEM_PROCESSOR} STREQUAL "arm64")
+    set(SlopeCraft_vectorize_flags -mfpu=neon)
+    #    if (${APPLE})
+    #        set(SlopeCraft_vectorize_flags -mcpu=apple-m1 -mfpu=neon)
+    #    endif ()
+else ()
+    message(WARNING "Unknown cpu arch, using -march=native")
+endif ()
+
+
+message(STATUS "Vectorize using " ${SlopeCraft_vectorize_flags})
+add_compile_options(${SlopeCraft_vectorize_flags})
