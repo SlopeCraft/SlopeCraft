74c1ec0a5c6901a51c23115455522b9b97a26575
ToKiNoBug <tokinobug@163.com>
2024-03-25 21:37:48 +0800

    update to nlohmann json 3.11.3
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- cmake/find_nlohmann_json.cmake
+++ cmake/find_nlohmann_json.cmake
@@ -1,31 +1,32 @@
 set(SlopeCraft_download_njson OFF)
 
-if(NOT DEFINED SlopeCraft_Nlohmann_json_include_dir)
-    if(EXISTS ${CMAKE_SOURCE_DIR}/3rdParty/nlohmann/json.hpp)
+if (NOT DEFINED SlopeCraft_Nlohmann_json_include_dir)
+    if (EXISTS ${CMAKE_SOURCE_DIR}/3rdParty/nlohmann/json.hpp)
         set(SlopeCraft_Nlohmann_json_include_dir ${CMAKE_SOURCE_DIR}/3rdParty/nlohmann)
-    else()
+    else ()
         set(SlopeCraft_download_njson ON)
-    endif()
-else()
+    endif ()
+else ()
     # check if cmake can find the json file
-    if(EXISTS ${SlopeCraft_Nlohmann_json_include_dir}/json.hpp) 
-    # nothing to do
-    else()
-        message(WARNING 
-            "The original value of SlopeCraft_Nlohmann_json_include_dir is invalid: failed to find " 
+    if (EXISTS ${SlopeCraft_Nlohmann_json_include_dir}/json.hpp)
+        # nothing to do
+    else ()
+        message(WARNING
+            "The original value of SlopeCraft_Nlohmann_json_include_dir is invalid: failed to find "
             ${SlopeCraft_Nlohmann_json_include_dir}/json.hpp)
         set(SlopeCraft_download_njson ON)
-    endif()
-endif()
+    endif ()
+endif ()
 
-if(${SlopeCraft_download_njson})
+if (${SlopeCraft_download_njson})
     message(STATUS "Downloading nlohmann json...")
-    file(DOWNLOAD 
-        "https://github.com/nlohmann/json/releases/download/v3.11.2/json.hpp" 
+    file(DOWNLOAD
+        "https://github.com/nlohmann/json/releases/download/v3.11.3/json.hpp"
         ${CMAKE_SOURCE_DIR}/3rdParty/nlohmann/json.hpp
+        HASH 9bea4c8066ef4a1c206b2be5a36302f8926f7fdc6087af5d20b417d0cf103ea6
         SHOW_PROGRESS)
 
     message(STATUS "nlohmann json downloaded")
     set(SlopeCraft_download_njson OFF)
     set(SlopeCraft_Nlohmann_json_include_dir ${CMAKE_SOURCE_DIR}/3rdParty/nlohmann)
-endif()
+endif ()
