8fab28aa194bb83b3c7e6f77e4419c6cebe2371f
ToKiNoBug <tokinobug@163.com>
2024-04-09 20:52:18 +0800

    [CI][mac] fix download DylibDeployer
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- cmake/required_deps/_DLLDeployer.cmake
+++ cmake/required_deps/_DLLDeployer.cmake
@@ -1,23 +1,23 @@
-function(SC_download url filename)
-    if (EXISTS ${filename})
-        file(SIZE ${filename} size)
-        if (${size} GREATER 0)
-            return()
-        endif ()
-
-        file(REMOVE ${filename})
-    endif ()
-
-    message(STATUS "Downloading ${url} ...")
-    file(DOWNLOAD ${url} ${filename}
-        SHOW_PROGRESS
-        REQUIRED)
-
-    file(SIZE ${filename} size)
-    if (${size} LESS_EQUAL 0)
-        message(FATAL_ERROR "Failed to download ${filename} from ${url}, downloaded file is empty")
-    endif ()
-endfunction(SC_download)
+#function(SC_download url filename)
+#    if (EXISTS ${filename})
+#        file(SIZE ${filename} size)
+#        if (${size} GREATER 0)
+#            return()
+#        endif ()
+#
+#        file(REMOVE ${filename})
+#    endif ()
+#
+#    message(STATUS "Downloading ${url} ...")
+#    file(DOWNLOAD ${url} ${filename}
+#        SHOW_PROGRESS
+#        REQUIRED)
+#
+#    file(SIZE ${filename} size)
+#    if (${size} LESS_EQUAL 0)
+#        message(FATAL_ERROR "Failed to download ${filename} from ${url}, downloaded file is empty")
+#    endif ()
+#endfunction(SC_download)
 
 set(DLLD_download_dir ${CMAKE_BINARY_DIR}/3rdParty/DLLDeployer)
 set(DLLD_file ${DLLD_download_dir}/DLLDeployer.cmake)
