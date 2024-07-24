bcbc5262d92773a046b581388e183d2fb707fdd6
ToKiNoBug <tokinobug@163.com>
2024-07-24 23:17:27 +0800

    new dep: libnbt++
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- cmake/required_deps/boost.cmake
+++ cmake/required_deps/boost.cmake
@@ -1,7 +1,10 @@
 find_package(Boost
-    COMPONENTS iostreams
+    COMPONENTS iostreams multi_array
     REQUIRED)
 
 if (NOT TARGET Boost::iostreams)
     message(FATAL_ERROR "Failed to find Boost::iostreams, install boost first")
+endif ()
+if (NOT TARGET Boost::multi_array)
+    message(FATAL_ERROR "Failed to find Boost::multi_array, install boost first")
 endif ()
---
+++ cmake/required_deps/libnbt++.cmake
@@ -0,0 +1,14 @@
+cmake_minimum_required(VERSION 3.29)
+set(NBT_BUILD_SHARED OFF CACHE BOOL "")
+set(NBT_BUILD_TESTS OFF CACHE BOOL "")
+FetchContent_Declare(libnbt++
+    GIT_REPOSITORY https://github.com/PrismLauncher/libnbtplusplus
+    GIT_TAG 23b955121b8217c1c348a9ed2483167a6f3ff4ad #Merge pull request #3 from TheKodeToad/max-depth-attempt2
+    OVERRIDE_FIND_PACKAGE
+    EXCLUDE_FROM_ALL)
+message(STATUS "Cloning libnbt++...")
+FetchContent_MakeAvailable(libnbt++)
+find_package(libnbt++ REQUIRED)
+
+find_package(zlib REQUIRED)
+target_link_libraries(nbt++ ZLIB::ZLIB)
