3f4601c782681bac75a7813cffd5af1a7363d63c
ToKiNoBug <tokinobug@163.com>
2024-08-09 01:59:32 +0800

    fix get_blocks of block list in SCL
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- SlopeCraftL/mc_block.cpp
+++ SlopeCraftL/mc_block.cpp
@@ -37,14 +37,19 @@ mc_block::mc_block() {
 
 size_t block_list::get_blocks(mc_block_interface **dst, uint8_t *dst_basecolor,
                               size_t capacity_in_elements) noexcept {
-  if (dst == nullptr || dst_basecolor == nullptr || capacity_in_elements <= 0) {
+  if (capacity_in_elements <= 0) {
+    return 0;
+  }
+  if (dst == nullptr and dst_basecolor == nullptr) {
     return 0;
   }
 
   size_t counter = 0;
   for (auto &ptr : this->m_blocks) {
-    dst[counter] = ptr.first.get();
-    dst_basecolor[counter] = ptr.second;
+    if (dst) dst[counter] = ptr.first.get();
+
+    if (dst_basecolor) dst_basecolor[counter] = ptr.second;
+
     counter++;
     if (counter >= capacity_in_elements) {
       return counter;
@@ -56,14 +61,17 @@ size_t block_list::get_blocks(mc_block_interface **dst, uint8_t *dst_basecolor,
 size_t block_list::get_blocks(const mc_block_interface **dst,
                               uint8_t *dst_basecolor,
                               size_t capacity_in_elements) const noexcept {
-  if (dst == nullptr || dst_basecolor == nullptr || capacity_in_elements <= 0) {
+  if (capacity_in_elements <= 0) {
+    return 0;
+  }
+  if (dst == nullptr and dst_basecolor == nullptr) {
     return 0;
   }
 
   size_t counter = 0;
   for (auto &ptr : this->m_blocks) {
-    dst[counter] = ptr.first.get();
-    dst_basecolor[counter] = ptr.second;
+    if (dst) dst[counter] = ptr.first.get();
+    if (dst_basecolor) dst_basecolor[counter] = ptr.second;
     counter++;
     if (counter >= capacity_in_elements) {
       return counter;
