1d402db3716dd2003f1617bc7fd7c3a33d7adc8e
ToKiNoBug <tokinobug@163.com>
2024-08-08 20:02:17 +0800

    fix linux build (disable testing of tl-expected)
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- cmake/required_deps/tl-expected.cmake
+++ cmake/required_deps/tl-expected.cmake
@@ -1,5 +1,6 @@
  find_package(tl-expected 1.1.0 QUIET)
  if (NOT tl-expected_FOUND)
+     set(EXPECTED_BUILD_TESTS OFF)
      include(FetchContent)
      message(STATUS "Downloading tl-expected...")
      FetchContent_Declare(tl-expected
