fd3ae46d07a6428137afa6236897588f0f21de2a
ToKiNoBug <tokinobug@163.com>
2025-12-14 22:14:01 +0800

    [CI]: Try to fix mingw build
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- utilities/StatMemory/others/test_stat_memory.cpp
+++ utilities/StatMemory/others/test_stat_memory.cpp
@@ -9,10 +9,10 @@ int main() {
     const auto sys_info = get_system_memory_info();
     if (sys_info) {
       const auto &val = sys_info.value();
-      std::print("System free memory: {}\n", val.free);
-      std::print("System total memory: {}\n", val.total);
+      std::println("System free memory: {}", val.free);
+      std::println("System total memory: {}", val.total);
     } else {
-      std::print("Failed to get system memory info:\n{}\n", sys_info.error());
+      std::println("Failed to get system memory info:\n{}", sys_info.error());
       fail_count++;
     }
   }
@@ -20,9 +20,9 @@ int main() {
   const auto self_info = get_self_memory_info();
   if (self_info) {
     const auto &val = self_info.value();
-    std::print("Memory used by this process: \"{}\"\n", val.used);
+    std::println("Memory used by this process: \"{}\"", val.used);
   } else {
-    std::print("Failed to get self memory info:\n{}\n", self_info.error());
+    std::println("Failed to get self memory info:\n{}", self_info.error());
     fail_count++;
   }
 
